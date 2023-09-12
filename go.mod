1a18dca4af64f511f7b288dc1bd1dfd13c7a3e1d
Clint Armstrong <clint@clintarmstrong.net>
2023-09-12 14:52:40 -0400

    more scaffolding

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git.clinta.me/clinta/boxboss
 
 go 1.21.0
+
+require golang.org/x/sync v0.3.0
--- state/state.go
+++ state/state.go
@@ -1,57 +1,59 @@
 package state
 
 import (
+	"sync"
 	"sync/atomic"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 )
-
-// Status indicates the status of a a state
-type Status uint64
-
-const (
-	NotRun Status = iota
-	Triggered
-	PreCheck
-	Checking
-	PostCheck
-	PreRun
-	Running
-	Succeeeded
-	Failed
-)
-
-func (s Status) IsRunning() bool {
-	if s == NotRun || s == Succeeeded || s == Failed {
-		return false
-	}
-	return true
-}
 
 // State is any state that can be run
 type State interface {
 	Check() (bool, error)
 	Run() (bool, error)
+	// Error value from last run
+	LastError() error
+	// End timestamp of last run
+	LastRun() time.Time
+	// Whether or not the state made changes during it's last run
+	Changed() bool
 
 	// provided by BaseState
-	Status() Status
+	Running() bool
+	setRunning(bool)
 	getTrigger() <-chan struct{}
+	preChecks() ([]func() error, func())
+	postChecks() ([]func() error, func())
+	postRuns() ([]func() error, func())
+	setLastError(error)
+	setLastRun(time.Time)
+	setChanged(bool)
 }
 
 // BaseState provides the basic functionality for states, if embedded into a state, state only needs to implement Check() and Run()
 // TODO: Better name for this, this is to be embedded in actual state plugins
 // This embedding will implement everything needed for the State interface, except for check and run
 type BaseState struct {
-	status  atomic.Uint64
-	trigger chan struct{}
+	lastError       error
+	lastRun         time.Time
+	changed         bool
+	trigger         chan struct{}
+	running         atomic.Bool
+	preChecksMutex  sync.Mutex
+	preChecks_      []func() error
+	postChecksMutex sync.Mutex
+	postChecks_     []func() error
+	postRunsMutex   sync.Mutex
+	postRuns_       []func() error
 }
 
-// Status returns the current status for this state
-func (b *BaseState) Status() Status {
-	return Status(b.status.Load())
+func (b *BaseState) Running() bool {
+	return b.running.Load()
 }
 
-// setStatus sets the current status of this state, and dispatches all the listener channels
-func (b *BaseState) setStatus(v Status) {
-	b.status.Store(uint64(v))
+func (b *BaseState) setRunning(v bool) {
+	b.running.Store(v)
 }
 
 // Adds a trigger. Triggers are only processed if the state is not in one of the running states. Close stop channel to remove trigger.
@@ -60,7 +62,7 @@ func (b *BaseState) AddTrigger(trigger <-chan struct{}, stop <-chan struct{}) {
 		for {
 			select {
 			case <-trigger:
-				if b.Status().IsRunning() {
+				if b.Running() {
 					// TODO: Log ignoring trigger because state is running
 					continue
 				}
@@ -76,6 +78,96 @@ func (b *BaseState) getTrigger() <-chan struct{} {
 	return b.trigger
 }
 
+func (b *BaseState) AddPreCheck(f func() error) {
+	b.preChecksMutex.Lock()
+	b.preChecks_ = append(b.preChecks_, f)
+	b.preChecksMutex.Unlock()
+}
+
+// gets the preChecks slice, and the function to runlock when done reading it
+func (b *BaseState) preChecks() ([]func() error, func()) {
+	b.preChecksMutex.Lock()
+	return b.preChecks_, b.preChecksMutex.Unlock
+}
+
+func (b *BaseState) AddPostCheck(f func() error) {
+	b.postChecksMutex.Lock()
+	b.postChecks_ = append(b.postChecks_, f)
+	b.postChecksMutex.Unlock()
+}
+
+// gets the postChecks slice, and the function to runlock when done reading it
+func (b *BaseState) postChecks() ([]func() error, func()) {
+	b.postChecksMutex.Lock()
+	return b.postChecks_, b.postChecksMutex.Unlock
+}
+
+func (b *BaseState) AddPreRun(f func() error) {
+	f = func() error {
+		if b.lastError != nil {
+			return nil
+		}
+		return f()
+	}
+	b.AddPostCheck(f)
+}
+
+func (b *BaseState) AddPostRun(f func() error) {
+	b.postRunsMutex.Lock()
+	b.postRuns_ = append(b.postRuns_, f)
+	b.postRunsMutex.Unlock()
+}
+
+// gets the postRuns slice, and the function to runlock when done reading it
+func (b *BaseState) postRuns() ([]func() error, func()) {
+	b.postRunsMutex.Lock()
+	return b.postRuns_, b.postRunsMutex.Unlock
+}
+
+func (b *BaseState) AddPostSuccess(f func() error) {
+	f = func() error {
+		if b.lastError != nil {
+			return nil
+		}
+		return f()
+	}
+	b.AddPostRun(f)
+}
+
+func (b *BaseState) AddPostFailure(f func() error) {
+	f = func() error {
+		if b.lastError == nil {
+			return nil
+		}
+		return f()
+	}
+	b.AddPostRun(f)
+}
+
+func (b *BaseState) setLastError(err error) {
+	b.lastError = err
+}
+
+func (b *BaseState) LastError() error {
+	return b.lastError
+}
+
+func (b *BaseState) setLastRun(t time.Time) {
+	b.lastRun = t
+}
+
+func (b *BaseState) LastRun() time.Time {
+	return b.lastRun
+}
+
+func (b *BaseState) setChanged(v bool) {
+	b.changed = v
+}
+
+func (b *BaseState) Changed() bool {
+	return b.changed
+}
+
 // ManageState listens for triggers and manages a state, should be launched in a goroutine
 func ManageState(s State) {
 	go manageState(s)
@@ -83,6 +175,73 @@ func ManageState(s State) {
 
 func manageState(s State) {
 	for range s.getTrigger() {
+		runState(s)
+	}
+}
+
+func runState(s State) {
+	s.setRunning(true)
+	s.setLastError(nil)
+	defer s.setRunning(false)
+	defer s.setLastRun(time.Now())
+	s.setChanged(false)
+	err := func() error {
+		{ // Run preChecks
+			g := new(errgroup.Group)
+			preChecks, unlock := s.preChecks()
+			for _, f := range preChecks {
+				g.Go(f)
+			}
+			unlock()
+			if err := g.Wait(); err != nil {
+				return err
+			}
+		}
+		{ // Run the check
+			check, err := s.Check()
+			if err != nil {
+				return err
+			}
+			if !check {
+				return nil
+				// TODO Debug log, no changes required
+			}
+		}
+		{ // Run the postChecks
+			g := new(errgroup.Group)
+			postChecks, unlock := s.postChecks()
+			for _, f := range postChecks {
+				g.Go(f)
+			}
+			unlock()
+			if err := g.Wait(); err != nil {
+				return err
+			}
+		}
+		// Run the state
+		changed, err := s.Run()
+		s.setChanged(changed)
+		return err
+	}()
+	s.setLastError(err)
+
+	// Run the postRuns
+	err = func() error {
+		g := new(errgroup.Group)
+		postRuns, unlock := s.postRuns()
+		for _, f := range postRuns {
+			g.Go(f)
+		}
+		unlock()
+		if err := g.Wait(); err != nil {
+			return err
+		}
+		return nil
+	}()
+
+	// Only use the postrun errors if the main run did not already set an error
+	if s.LastError() == nil && err != nil {
+		s.setLastError(err)
 	}
 }
 
