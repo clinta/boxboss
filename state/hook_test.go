2bf5fadfdd2c1ab44e7a6028043a6598ba283d12
Clint Armstrong <clint@clintarmstrong.net>
2023-09-17 23:15:10 -0400

    aftercheck does not behave as expected, test proves

--- state/hook_test.go
+++ state/hook_test.go
@@ -20,7 +20,9 @@ func TestBeforeCheckFail(t *testing.T) {
 	_ = runner.AddBeforeCheckHook("testBeforeCheckHook", func(ctx context.Context) error {
 		return bfErr
 	})
-	assert.ErrorIs(runner.Apply(ctx), bfErr)
+	err := runner.Apply(ctx)
+	assert.ErrorIs(err, bfErr)
+	assert.ErrorIs(err, ErrBeforeCheckHook)
 	res := runner.Result(ctx)
 	assert.False(res.Changed())
 	assert.ErrorIs(res.Err(), bfErr)
@@ -74,6 +76,101 @@ func TestBeforeCheckRemove(t *testing.T) {
 	assert.Nil(runner.Apply(ctx))
 	assert.Equal(beforeHookTime, bft)
 
+	cancel()
+	goleak.VerifyNone(t)
+}
+
+func TestAfterCheckFail(t *testing.T) {
+	assert := assert.New(t)
+	ctx, cancel, state, runner := newTestRunner()
+	state.retCheckChanges = true
+	state.retRunChanges = true
+
+	var testErr = errors.New("testAfterCheckHookErr")
+	_ = runner.AddAfterCheckHook("testAfterCheckHook", func(ctx context.Context, _ bool, _ error) error {
+		return testErr
+	})
+	err := runner.Apply(ctx)
+	assert.ErrorIs(err, testErr)
+	assert.ErrorIs(err, ErrAfterCheckHook)
+	res := runner.Result(ctx)
+	assert.False(res.Changed())
+	assert.ErrorIs(res.Err(), testErr)
+	assert.NotZero(res.Completed())
+	assert.Equal(len(state.checks), 1)
+	assert.Zero(state.runs)
+	cancel()
+	goleak.VerifyNone(t)
+}
+
+func TestAfterCheckSucceed(t *testing.T) {
+	assert := assert.New(t)
+	ctx, cancel, state, runner := newTestRunner()
+	beforeHookTime := time.Time{}
+
+	_ = runner.AddAfterCheckHook("testAfterCheckHook", func(ctx context.Context, _ bool, _ error) error {
+		beforeHookTime = time.Now()
+		return nil
+	})
+	assert.Nil(runner.Apply(ctx))
+	res := runner.Result(ctx)
+	assert.False(res.Changed())
+	assert.Nil(res.Err())
+	assert.NotZero(res.Completed())
+	assert.True(beforeHookTime.After(state.checks[0]))
+	assert.Zero(state.runs)
+	cancel()
+	goleak.VerifyNone(t)
+}
+
+func TestAfterCheckRemove(t *testing.T) {
+	assert := assert.New(t)
+	ctx, cancel, state, runner := newTestRunner()
+	beforeHookTime := time.Time{}
+
+	rmBf := runner.AddAfterCheckHook("testAfterCheckHook", func(ctx context.Context, _ bool, _ error) error {
+		beforeHookTime = time.Now()
+		return nil
+	})
+	assert.Nil(runner.Apply(ctx))
+	res := runner.Result(ctx)
+	assert.False(res.Changed())
+	assert.Nil(res.Err())
+	assert.NotZero(res.Completed())
+	assert.True(beforeHookTime.After(state.checks[0]))
+	bft := beforeHookTime
+	assert.Zero(state.runs)
+
+	rmBf()
+
+	assert.Nil(runner.Apply(ctx))
+	assert.Equal(beforeHookTime, bft)
+
+	cancel()
+	goleak.VerifyNone(t)
+}
+
+func TestAfterCheckCheckFaild(t *testing.T) {
+	assert := assert.New(t)
+	ctx, cancel, state, runner := newTestRunner()
+	state.retCheckErr = errors.New("check failed")
+
+	afcRan := false
+	_ = runner.AddAfterCheckHook("testAfterCheckHook", func(ctx context.Context, _ bool, err error) error {
+		assert.ErrorIs(err, state.retCheckErr)
+		afcRan = true
+		return nil
+	})
+	err := runner.Apply(ctx)
+	assert.ErrorIs(err, state.retCheckErr)
+	assert.ErrorIs(err, ErrCheckFailed)
+	res := runner.Result(ctx)
+	assert.False(res.Changed())
+	assert.ErrorIs(res.Err(), state.retCheckErr)
+	assert.NotZero(res.Completed())
+	assert.Equal(len(state.checks), 1)
+	assert.True(afcRan)
+	assert.Zero(state.runs)
 	cancel()
 	goleak.VerifyNone(t)
 }
